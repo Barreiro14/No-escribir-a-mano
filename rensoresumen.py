faced5efe1cceb9f20392ba9af20a52185faf58e
Gabriel Barreiro <elbarri@omar-pc>
2021-09-10 12:59:09 -0400

    first loop over leters

--- rensoresumen.py
+++ rensoresumen.py
@@ -1,3 +1,20 @@
 from PIL import Image
 
-
+ma = Image.open("a.png") #nombre de la imagen de la letra a
+#TODO: crear las imagenes para cada letra.
+#TODO: enable on production
+#mA = ma.resize((n, m)) #letra mayuscula
+#ma = ma.resize((c, q)) #cambiar el tamano de la letra minuscula
+#TODO: determinar el size de las imagenes tanto para minuscula y mayuscula
+#TODO: crear la imagen del size del texto
+#TODO: calcular el size de de la imagen resultante a partir de la cantidad de letras.
+def cambio(f):
+    F = open(f, "r")
+    i = 0
+    j = 0
+    lines = F.readlines()
+    for line in lines:
+        while i < len(F[0]):
+            while j < len(F):
+                if F[i][j] == "a":
+                    #cambiar por la imagen a
